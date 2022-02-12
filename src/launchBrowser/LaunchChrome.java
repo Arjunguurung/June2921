0eebe7b18a02a8aab9e67606b3c92bf874b6dfc9
Arjun Gurung <thearjungurung@gmail.com>
2022-02-12 14:40:37 -0600

    First commit

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,16 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		
+//	System.setProperty("webdriver.chrome.driver", "C:C:\\Users\\thema\\QAJune2021\\1st_SeleniumProject\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	}
+}
